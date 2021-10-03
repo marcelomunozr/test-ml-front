20004ab5a8fbef3a00eb41db26dbfa963eb5bf9b
Marcelo Enrique Muñoz Rodriguez <marcelo.e.munoz@gmail.com>
2021-10-03 18:08:02 -0300

    feat: se agrega vista descripcion de producto

--- src/app/pages/Producto.js
+++ src/app/pages/Producto.js
@@ -1,22 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { getDetalleProducto, getProducto } from '../services/products.services';
+import { getProducto } from '../services/products.services';
 
 const Producto = () => {
     const [loading, setLoading] = useState(true);
     const [errorData, setErrorData] = useState(false);
     const [productData, setProductData] = useState(null);
-    const [productDescripcion, setProductDescripcion] = useState(null);
     const { id } = useParams();
 
     const handleGetDataProducto = async () => {
         try {
             const product = await getProducto(id);
-            const description = await getDetalleProducto(id);
-            if (product.status === 200 && description.status === 200) {
-                if (product.data && description.data) {
+            if (product.status === 200) {
+                if (product.data) {
                     setProductData(product.data)
-                    setProductDescripcion(description.data)
                 }
             }
         } catch (error) {
@@ -42,23 +39,32 @@ const Producto = () => {
         }
         const {
             title,
-            thumbnail,
-        } = productData;
-        console.log('productData', productData);
-        console.log('productDescripcion', productDescripcion);
+            picture,
+            condition,
+            price: { amount },
+            free_shipping,
+            sold_quantity,
+            description,
+        } = productData.item;
+        console.log('productData.item', productData.item);
+        const estadoProducto = condition === 'new' ? 'Nuevo' : 'Usado';
+        const envioGratis = free_shipping ? 'Sí' : 'No';
         return (
             <div className="single-product">
                 <div className="item-area">
                     <div className="left-area">
-                        <img width="40%" src={thumbnail} className="ui-search-result-image__element" alt={title} />
+                        <img width="40%" src={picture} className="ui-search-result-image__element" alt={title} />
                     </div>
                     <div className="right-area">
-                        {/* <h2>${amount.toLocaleString("es-AR")}</h2> */}
-                        <p>{title}</p>
+                        <p className="fs_12 mb_0">{estadoProducto} - {sold_quantity} Vendidos</p>
+                        <h1 className="mt_0">{title}</h1>
+                        <h4 className="price">${amount.toLocaleString("es-AR")}</h4>
+                        <p><strong>Envio gratis: </strong>{envioGratis}</p>
+                        <button type="submit" class="ml-button primary" formaction="https://www.mercadolibre.cl/gz/checkout/buy"><span class="andes-button__content">Comprar ahora</span></button>
                     </div>
                 </div>
-                <div className="description-area">
-                    {/* <h4>{state_name}</h4> */}
+                <div className="item-area">
+                    <p className="fs_14">{description}</p>
                 </div>
             </div>
         );
